5351d7142712bb006b435c5b8c93e0d47d15bd9d
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-09 11:46:30 +1000

    The Main Menu

--- Week 6/draw.js
+++ Week 6/draw.js
@@ -5,7 +5,13 @@ BGImage.src = "Images/Background.png";
 
 function Draw()
 {
+<<<<<<< HEAD
     context.fillStyle = "blue";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(BGImage, 0, 0);
+    context.drawImage(BGImage, 0, 0, 1650, 870);
+=======
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, c.width, c.height);
+    ctx.drawImage(BGImage, 0, 0);
+>>>>>>> 9a2cfdcd58c1e7c56be25a78852eeabe09251336
 }
