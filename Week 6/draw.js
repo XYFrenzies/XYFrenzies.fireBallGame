0b10a7af58243b22b419bc8f396c755475aeaf8f
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-09 12:22:49 +1000

    Added character image

--- Week 6/draw.js
+++ Week 6/draw.js
@@ -5,10 +5,10 @@ BGImage.src = "Images/Background.png";
 
 function Draw()
 {
+    //Draw background
     ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.drawImage(BGImage, 0, 0, 1650, 870);
-
-
-
+    //Draw player
+    ctx.drawImage(player.image, player.x, player.y);
 }
--- Week 6/main.js
+++ Week 6/main.js
@@ -3,7 +3,10 @@ var c = document.getElementById("gamecanvas");
 var ctx = c.getContext("2d");
 
 
+//Initialisation of global objects
 
+//Player object
+var player = new Player();
 
 //Main game loop
 function Gameloop()
