17cb3872f69316dd2b20b0ef6e9e1f7e891778fb
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-09 12:17:36 +1000

    asdf

--- Week 6/draw.js
+++ Week 6/draw.js
@@ -6,6 +6,7 @@ BGImage.src = "Images/Background.png";
 function Draw()
 {
     ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, c.width, c.height);
     ctx.drawImage(BGImage, 0, 0, 1650, 870);
 
     ctx.fillStyle = "blue";
