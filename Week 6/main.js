8a3fa17fe042d3cf151688c44b8fba00f0c7529d
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-09 12:31:58 +1000

    Added character movement

--- Week 6/main.js
+++ Week 6/main.js
@@ -5,13 +5,29 @@ var ctx = c.getContext("2d");
 
 //Initialisation of global objects
 
+//Keyboard Listener object
+var listener = new window.keypress.Listener();
+listener.simple_combo("s", function() {
+    player.y += 5;
+});
+listener.simple_combo("d", function() {
+    player.x += 5;
+});
+listener.simple_combo("w", function() {
+    player.y -= 5;
+});
+listener.simple_combo("a", function() {
+    player.x -= 5;
+});
 //Player object
 var player = new Player();
+
 
 //Main game loop
 function Gameloop()
 {
     Draw();
+    window.requestAnimationFrame(Gameloop)
 }
 
 //Init game
