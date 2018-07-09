2bfdff83c74e792cb2871077349c969b3d6a57ec
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-10 09:27:50 +1000

    Put in the buildanimation in player object

--- Week 6/playerObject.js
+++ Week 6/playerObject.js
@@ -68,4 +68,9 @@ function Player()
         "this"          : this
     }
   ]);
+
+  this.sprite = new Sprite("Sprites/Sprite Sheet.png");
+
+  this.sprite.buildAnimation(5, 1, 54, 73, 0.2 [6, 6, 7, 7, 8, 8, 9, 9, 10, 10]); //Moving Left
+  this.sprite.buildAnimation(5, 1, 54, 73, 0.2 [1, 1, 2, 2, 3, 3, 4, 4, 5, 5,]); //Moving Right
 }
