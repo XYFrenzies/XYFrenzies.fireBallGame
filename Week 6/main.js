611abd92371669ef8412f6fa21394dc27b9c92c4
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-09 13:06:44 +1000

    Smooth movement

--- Week 6/main.js
+++ Week 6/main.js
@@ -5,20 +5,6 @@ var ctx = c.getContext("2d");
 
 //Initialisation of global objects
 
-//Keyboard Listener object
-var listener = new window.keypress.Listener();
-listener.simple_combo("s", function() {
-    player.y += 5;
-});
-listener.simple_combo("d", function() {
-    player.x += 5;
-});
-listener.simple_combo("w", function() {
-    player.y -= 5;
-});
-listener.simple_combo("a", function() {
-    player.x -= 5;
-});
 //Player object
 var player = new Player();
 
@@ -27,6 +13,7 @@ var player = new Player();
 function Gameloop()
 {
     Draw();
+    movement();
     window.requestAnimationFrame(Gameloop)
 }
 
---
+++ Week 6/movement.js
@@ -0,0 +1,15 @@
+function movement()
+{
+  if (player.jumping) {
+    player.y--;
+  }
+  if (player.crouching) {
+    player.y++;
+  }
+  if (player.movingRight) {
+    player.x++;
+  }
+  if (player.movingLeft) {
+    player.x--;
+  }
+}
