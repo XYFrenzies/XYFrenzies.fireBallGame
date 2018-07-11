9edd08ae176e16b385b98dacfd958ef16fca5c04
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-11 11:56:30 +1000

    Revert "Merge branch 'master' of https://github.com/XYFrenzies/XYFrenzies.github.io"
    This reverts commit 33c8766d5a6916d928080b285e33aba2c2673ebe, reversing
    changes made to 98a12aac656a0f271ef8320390d072d79745c5df.

--- Week 7/maybe just/Enemy.js
+++ Week 7/maybe just/Enemy.js
@@ -11,11 +11,9 @@ var Enemy = function()
     this.sprite = new Sprite("Images/Enemy sprite sheet.png");
 
 
-    this.sprite.buildAnimation(8,2,131,93,0.2,[0,1,2,1]);
+    this.sprite.buildAnimation(8,2,131,93,0.5,[0,1,2]);
 
-    this.sprite.buildAnimation(8,2,131,93,0.2,[7,6,6,5]);
-
-    this.sprite.buildAnimation(8,2,131,93,1, [9,9,9,10,11,12])
+    this.sprite.buildAnimation(8,2,131,93,1,[7,6,5]);
 
     this.isFalling = true;
 
@@ -42,18 +40,10 @@ Enemy.prototype.Update = function()
         if(this.x > player.x)
         {
             this.x -= 2;
-            if(this.sprite.currentAnimation != 0)
-            {
-                this.sprite.setAnimation(0);
-            }
         }
         if(this.x < player.x)
         {
             this.x += 2;
-            if(this.sprite.currentAnimation != 1)
-            {
-                this.sprite.setAnimation(1);
-            }
         }
     }
     this.sprite.update(dt);
