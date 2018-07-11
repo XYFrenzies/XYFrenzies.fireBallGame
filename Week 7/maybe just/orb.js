ecc45e974e59531defc82e073cc9ccbcb7af0913
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-11 10:40:24 +1000

    ughhhhhhhhhhhhhhhhhhhhh

--- Week 7/maybe just/orb.js
+++ Week 7/maybe just/orb.js
@@ -2,11 +2,11 @@ function ORB(inX, inY, inDir){
   this.x = inX;
   this.y = inY;
   this.dir = inDir;
-  //this.sprite = new Sprite("Images/player orb.png");
-  this.image = document.createElement("img");
-  this.image.src = ("Images/player orb.png");
+  this.sprite = new Sprite("Images/player orb.png");
+  //this.image = document.createElement("img");
+  //this.image.src = ("Images/player orb.png");
 
-  //this.sprite.buildAnimation(2, 1, 45, 45, 0.2, [0, 0, 1, 1]);
+  this.sprite.buildAnimation(2, 1, 45, 45, 0.2, [0, 1]);
 }
 
 ORB.prototype.Update = function()
@@ -18,7 +18,9 @@ ORB.prototype.Update = function()
 ORB.prototype.Draw = function()
 {
     //this.sprite.draw(context, this.x, this.y);
-    context.drawImage(this.image, this.x - player.x, this.y - player.y);
+    //context.drawImage(this.image, this.x - player.x, this.y - player.y);
+
+    this.sprite.draw(context, this.x - player.x, this.y - player.y);
     //context.fillStyle("black");
     //context.fillStyle = "black";
     //context.fillRect(this.x - player.x, this.x - player.y, 150, 150);
