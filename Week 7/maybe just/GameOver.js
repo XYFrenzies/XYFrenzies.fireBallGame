752ca59207f40ef3d4bcf57396b7615c5a2dbfbf
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-11 15:13:22 +1000

    GameOver png

--- Week 7/maybe just/GameOver.js
+++ Week 7/maybe just/GameOver.js
@@ -2,7 +2,7 @@ function DrawGameOver()
 {
 
   var MainMenuBG = document.createElement("img");
-  MainMenuBG.src = "Images/WOZTheFinalWizard.png";
+  MainMenuBG.src = "Images/GameOver.png";
 
   context.drawImage(MainMenuBG, 0,0,canvas.width, canvas.height);
 
