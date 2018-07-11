1e4435b141b9ce08b5951f1d32537d26326b5c95
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-11 14:12:06 +1000

    Menu

--- Week 7/maybe just/Menu.js
+++ Week 7/maybe just/Menu.js
@@ -1,9 +1,17 @@
 function DrawMenu()
 {
+  context.fillStyle = "Blue";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "Gold";
+  context.fillText("Welcome to wizard baller!", canvas.width / 4, canvas.height / 6);
+  context.fillText("Press space to begin!", canvas.width / 4, canvas.height / 2 - 15)
 
 }
 
 function UpdateMenu()
 {
-  
+  if(keyboard.isKeyDown(keyboard.KEY_SPACE))
+  {
+    Level = Level1;
+  }
 }
