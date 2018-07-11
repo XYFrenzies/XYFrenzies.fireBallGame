2fdbecbd73f119d59d070ab532de4d3b3674b9d0
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-11 10:03:07 +1000

    Changed capitalisation

--- Week 7/maybe just/orb.js
+++ Week 7/maybe just/orb.js
@@ -2,7 +2,7 @@ function ORB(inX, inY, inDir){
   this.x = inX;
   this.y = inY;
   this.dir = inDir;
-  this.sprite = new Sprite("images/player orb.png");
+  this.sprite = new Sprite("Images/player orb.png");
 
   this.sprite.buildAnimation(2, 1, 45, 45, 0.2, [0, 0, 1, 1]);
 
