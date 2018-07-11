286b0789a345cb4652ca83837531aa8b29c96bd6
DiabloWolf <38517437+DiabloWolf@users.noreply.github.com>
2018-07-11 12:24:39 +1000

    Revert "somewhere"
    This reverts commit e77845b970015412080bb3310a5c31c8d40505ff.

--- Week 7/maybe just/Enemy.js
+++ Week 7/maybe just/Enemy.js
@@ -11,13 +11,11 @@ var Enemy = function()
     this.sprite = new Sprite("Images/Enemy sprite sheet.png");
 
 
-    this.sprite.buildAnimation(8,2,131,93,0.2,[0,1,2]);
+    this.sprite.buildAnimation(8,2,131,93,0.5,[0,1,2]);
 
-    this.sprite.buildAnimation(8,2,131,93,0.2,[7,6,5]);
+    this.sprite.buildAnimation(8,2,131,93,1,[7,6,5]);
 
     this.sprite.buildAnimation(8,2,131,93,0.1, [8,8,8,9,10,11])
-
-    this.sprite.buildAnimation(8,2,131,93,0.1, [15,15,15,14,13,12])
 
     this.isFalling = true;
 
@@ -26,19 +24,9 @@ var Enemy = function()
 
 Enemy.prototype.Attack = function()
 {
-    if(this.sprite.currentAnimation == 0)
+    if(this.sprite.currentAnimation != 2)
     {
-        if(this.sprite.currentAnimation != 2)
-        {
-            this.sprite.setAnimation(2);
-        }
-    }
-    if(this.sprite.currentAnimation == 1)
-    {
-        if(this.sprite.currentAnimation != 3)
-        {
-            this.sprite.setAnimation(3);
-        }
+        this.sprite.setAnimation(2);
     }
 }
 
--- Week 7/maybe just/collision.js
+++ Week 7/maybe just/collision.js
@@ -1,5 +1,7 @@
 function collides()
 {
+  if(enemies.length > 0)
+  {
   for(i = 0; i < enemies.length; i++)
   {
     var r1 = 18;
@@ -7,13 +9,20 @@ function collides()
     var deleted = 0;
     if(Math.sqrt( (player.x - enemies[i].x) * (player.x - enemies[i].x) + (player.y - enemies[i].y) * (player.y - enemies[i].y) ) < (r1 + r2))
     {
+      lives -= 1;
       //Trigger
       enemies[i].Attack();
     }
+    if (orbs.length > 0)
+    {
     for(l = 0; l < orbs.length; l++)
     {
       var r3 = 5;
       var r4 = 5;
+      if(enemies.length <= 0)
+      {
+        break;
+      }
       if(Math.sqrt( (orbs[l].x - (enemies[i].x + 25)) * (orbs[l].x - (enemies[i].x + 25)) + (orbs[l].y - (enemies[i].y + 25)) * (orbs[l].y - (enemies[i].y + 25)) ) < (r3 + r4))
       {
         delete enemies[i];
@@ -22,9 +31,9 @@ function collides()
         score += 100;
         enemies.length -= deleted;
         i--;
-
       }
     }
-
+    }
   }
+}
 }
