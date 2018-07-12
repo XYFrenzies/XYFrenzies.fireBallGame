d33bd41df1b2b5297a382ce54ee86d9ee4f0d21d
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-12 12:17:02 +1000

    COMMENTS

--- Week 7/maybe just/GameOver.js
+++ Week 7/maybe just/GameOver.js
@@ -1,6 +1,7 @@
 function DrawGameOver()
 {
-
+//This is the gameOver screen of which when the player
+//loses all of the lives, this screen appears.
   var MainMenuBG = document.createElement("img");
   MainMenuBG.src = "Images/GameOver.png";
 
@@ -9,7 +10,9 @@ function DrawGameOver()
   context.fillText("You got a score of: "+ score, canvas.width / 2 - 165, canvas.height / 2 - 60);
 
 }
-
+//On button press with the enter key,
+//the individual is sent to the main menu.
+//As well as this, the game state is reloaded.
 function UpdateGameOver()
 {
   if(keyboard.isKeyDown(keyboard.KEY_ENTER))
--- Week 7/maybe just/collision.js
+++ Week 7/maybe just/collision.js
@@ -1,9 +1,11 @@
 function collides()
 {
+  //This involves the collision of the enemy with the player.
   if(enemies.length > 0)
   {
   for(i = 0; i < enemies.length; i++)
   {
+    //Checks for the radius of the enemy using circle to circle collision.
     var r1 = 40;
     var r2 = 40;
     var deleted = 0;
@@ -11,6 +13,8 @@ function collides()
     {
     if(Math.sqrt( (player.x - enemies[i].x) * (player.x - enemies[i].x) + (player.y - enemies[i].y) * (player.y - enemies[i].y) ) < (r1 + r2))
     {
+      //This checks that if the player is within the range of the enemy,
+      //the lives will decrease by 1 and that the enemy attack will occur.
       if(superTime > 0)
       {
 
@@ -25,6 +29,7 @@ function collides()
     }
     if (orbs.length > 0)
     {
+    //This is the collision of the orbs with the enemies.
     for(l = 0; l < orbs.length; l++)
     {
       var r3 = 5;
@@ -35,10 +40,10 @@ function collides()
         {
           if(Math.sqrt( (orbs[l].x - (enemies[i].x + 25)) * (orbs[l].x - (enemies[i].x + 25)) + (orbs[l].y - (enemies[i].y + 25)) * (orbs[l].y - (enemies[i].y + 25)) ) < (r3 + r4))
           {
+            //If the orb collides with the enemy, the enemy will delete as well as the orb.
             delete enemies[i];
             delete orbs[l];
-            //enemies.length--;
-            //i--;
+            //The score will increase by 100
             score += 100;
           }
         }
@@ -50,7 +55,7 @@ function collides()
 }
 }
 }
-  //Dont play with fire, (Dont touch my fireball code)
+  //This code is to get the wizard fireball to interact with the player and as a result, they lose a life.
   for(i = 0; i < fireBalls.length; i++)
   {
     var r5 = 10;
@@ -61,11 +66,10 @@ function collides()
       {
         if(Math.sqrt( (fireBalls[i].x - (player.x + 25)) * (fireBalls[i].x - (player.x + 25)) + (fireBalls[i].y - (player.y + 25)) * (fireBalls[i].y - (player.y + 25)) ) < (r5 + r6))
         {
+        //This is the check if the player is interacting with the wizards fireball.
+        //The lives will decrease by 1 and that the fireball will delete from the game.  
           player.Lives -= 1;
           delete fireBalls[i];
-          //enemies.length--;
-          //i--;
-          score += 100;
         }
       }
 
