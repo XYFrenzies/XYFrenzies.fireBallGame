7caf647aab41f8917063ab94e296604df6c3d0d2
jaydenhawkes123 <jayden@jaydenhawkes.com>
2018-07-12 10:29:57 +1000

    Bug removals

--- Week 7/maybe just/Wizard.js
+++ Week 7/maybe just/Wizard.js
@@ -1,7 +1,7 @@
 var Wizard = function()
 {
-
-
+    this.img = document.createElement("img");
+    this.img.src = "Images/Summoner.png";
     this.x = Math.random() * 6179 - 496;
     //right
     if( this.x <= player.x + 500 && this.x >= player.x )
@@ -28,38 +28,10 @@ var Wizard = function()
     this.width = 188/4;
     this.height = 241/4;
     this.cooldown = 8;
-    this.sprite = new Sprite("Images/Enemy sprite sheet.png");
-
-
-    this.sprite.buildAnimation(8,2,131,93,0.2,[0,1,2]);
-
-    this.sprite.buildAnimation(8,2,131,93,0.2,[7,6,5]);
-
-    this.sprite.buildAnimation(8,2,131,93,0.1, [8,8,8,9,10,11])
-
-    this.sprite.buildAnimation(8,2,131,93,0.1, [15,15,15,14,13,12])
-
-
-
 }
 
 Wizard.prototype.Attack = function()
 {
-    if(this.sprite.currentAnimation == 0)
-    {
-        this.sprite.setAnimation(2);
-        if(this.sprite.currentAnimation != 2)
-        {
-            this.sprite.setAnimation(2);
-        }
-    }
-    if(this.sprite.currentAnimation == 1)
-    {
-        if(this.sprite.currentAnimation != 3)
-        {
-            this.sprite.setAnimation(3);
-        }
-    }
     fireBalls.push(new FireBall(this.x, this.y, (this.x - player.x), (this.y - player.y)))
 }
 
@@ -80,31 +52,10 @@ Wizard.prototype.Update = function()
       this.Attack();
       this.cooldown = 8;
     }
-
-    if(this.x - 70 >= player.x)
-    {
-        this.x -= 2;
-        if(this.sprite.currentAnimation != 0)
-        {
-            this.sprite.setAnimation(0);
-        }
-    }
-    if(this.x + 50 < player.x)
-    {
-        this.x += 2;
-        if(this.sprite.currentAnimation != 1)
-        {
-            this.sprite.setAnimation(1);
-        }
-    }
     this.cooldown - dt;
-    this.sprite.update(dt);
 }
 
 Wizard.prototype.Draw = function()
 {
-
-    this.sprite.draw(context,this.x - player.x + 600, this.y - player.y + 340);
-
-
+    context.drawImage(this.img, this.x, this.y)
 }
