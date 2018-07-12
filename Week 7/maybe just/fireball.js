9565392a9c7ee52149ecfe5eb8a7f736af8d35a5
XYFrenzies <38517312+XYFrenzies@users.noreply.github.com>
2018-07-12 12:58:54 +1000

    COMMENTS

--- Week 7/maybe just/fireball.js
+++ Week 7/maybe just/fireball.js
@@ -3,12 +3,12 @@ function FireBall(inX, inY, inDirX){
   this.y = inY;
   this.dirX = inDirX;
   this.sprite = new Sprite("Images/Fireball Sprite sheet.png");
-  //this.image = document.createElement("img");
-  //this.image.src = ("Images/player orb.png");
 
+//This is the animation of the sprite of the fireball
+//of which changes from one frame to another.
   this.sprite.buildAnimation(2, 1, 45, 45, 0.1, [0, 1]);
 }
-
+//This is to 
 FireBall.prototype.Update = function()
 {
   this.x += this.dirX * dt;
@@ -17,16 +17,6 @@ FireBall.prototype.Update = function()
 
 FireBall.prototype.Draw = function()
 {
-    //this.sprite.draw(context, this.x, this.y);
-
-    //context.drawImage(this.image, this.x - player.x + 300, this.y - player.y + 300);
-
-    //context.drawImage(this.image, this.x - player.x, this.y - player.y);
-
     this.sprite.draw(context, this.x - player.x, this.y - player.y);
 
-    //context.fillStyle("black");
-    //context.fillStyle = "black";
-    //context.fillRect(this.x - player.x, this.x - player.y, 150, 150);
-  //Draw code
 }
